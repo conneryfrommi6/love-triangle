f050e1abea22bcbc953c4cbc276b2efd8bfcbb6c
conneryfrommi6 <47834023+conneryfrommi6@users.noreply.github.com>
2019-03-09 12:16:53 +0300

    add result

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
 /**
- * @param preferences - an array of integers. Indices of people, whom they love
+ * @param preferences -
+ *            an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+
+
+module.exports = function getLoveTrianglesCount(mass = []) {
+ 
+	var count = 0;
+	for(let arr=[], n=0; n < mass.length; n++){
+		if(!(arrSet(arr, mass[n]) || mass[n]-1===n || mass[n] === mass[mass[n]-1])){
+			let a = mass[n];
+			let b = mass[a-1];
+			let c = mass[b-1];
+			let d = mass[c-1];
+			if(a===d){
+				arr[arr.length]=a;
+				arr[arr.length]=b;
+				arr[arr.length]=c;
+				count++;		
+			}
+		}
+	}
+	return count;
+	};
+
+
+function arrSet(arr=[], elem){
+	for(let is, k = 0; k < arr.length; k++){
+		if(arr[k] === elem) {
+			return true;
+		}
+	}
+	return false;
+}
